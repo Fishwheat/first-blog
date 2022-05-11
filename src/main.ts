7e61363e71881ebd3a608c03cb1c52f0028e10de
coder.zhang <coder.zhang@accentrix.com>
2022-05-11 10:18:09 +0800

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,16 @@ import zhCn from 'element-plus/es/locale/lang/zh-cn';
 import 'element-plus/dist/index.css';
 // import './theme'; // 更换主题
 // import locale from '/element-ui/lib/locale/lang/zh-CN';
-import axios from 'axios';
+// import axios from 'axios';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import ApiClient from './apis/api-client';
+// import ApiClient from './apis/api-client';
 import VueI18n from './language';
 
-ApiClient.setInstance(axios.create({
-  baseURL: '/api',
-}));
+// ApiClient.setInstance(axios.create({
+//   baseURL: '/api',
+// }));
 
 const app = createApp(App);
 app.use(ElementPlus, { locale: zhCn });
